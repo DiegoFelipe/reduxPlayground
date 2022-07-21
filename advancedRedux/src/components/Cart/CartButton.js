7057108eed99b9fa4a18c6d1f6fe35453948b6a0
Diego Felipe <diegofelipe.14@gmail.com>
2022-07-21 16:32:26 -0400

    setup advanced redux pt1

---
+++ advancedRedux/src/components/Cart/CartButton.js
@@ -0,0 +1,18 @@
+import classes from "./CartButton.module.css";
+import { useDispatch } from "react-redux";
+import { uiActions } from "../../store/slices/ui";
+
+const CartButton = (props) => {
+  const dispatch = useDispatch();
+  const toggleCart = () => {
+    dispatch(uiActions.toggle());
+  };
+  return (
+    <button className={classes.button} onClick={toggleCart}>
+      <span>My Cart</span>
+      <span className={classes.badge}>1</span>
+    </button>
+  );
+};
+
+export default CartButton;
