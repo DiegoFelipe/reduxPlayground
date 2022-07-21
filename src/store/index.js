15025416b5b6307947dfd5ce6af2f91198ff370a
Diego Felipe <diegofelipe.14@gmail.com>
2022-07-21 00:44:26 -0400

    Adding state slice & redux toolkit

--- src/store/index.js
+++ src/store/index.js
@@ -1,4 +1,29 @@
 import { createStore } from "redux";
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  conter: 0,
+  showCounter: true,
+};
+
+createSlice({
+  name: "counter",
+  initialState,
+  reducers: {
+    increment(state) {
+      state.conter++; // doesn't actually chage the state it seems to, because redux toolkit automatically fix it internally
+    },
+    decrement(state) {
+      state.counter--;
+    },
+    increase(state, payload) {
+      state.counter += payload;
+    },
+    toggle(state) {
+      state.showCounter = !state.showCounter;
+    },
+  },
+});
 
 const reducer = (state = { counter: 0 }, action) => {
   if (action.type === "increment") {
