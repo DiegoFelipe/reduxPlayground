182985d2ec8d38c8a8074238526b456f629febe3
Diego Felipe <diegofelipe.14@gmail.com>
2022-08-03 10:26:47 -0400

    Using token on protected resources

--- s22-auth/src/components/Profile/ProfileForm.js
+++ s22-auth/src/components/Profile/ProfileForm.js
@@ -1,17 +1,48 @@
-import classes from './ProfileForm.module.css';
+import classes from "./ProfileForm.module.css";
+import { useContext, useRef } from "react";
+import AuthCtx from "../../store/auth-context";
+import { FIREBASE_KEY } from "../../consts/firebase";
 
 const ProfileForm = () => {
+  const authContext = useContext(AuthCtx);
+  const newPassRef = useRef();
+
+  const submitHandler = (event) => {
+    event.preventDefault();
+
+    const enteredNewPassword = newPassRef.current.value;
+
+    fetch(
+      `https://identitytoolkit.googleapis.com/v1/accounts:update?key=${FIREBASE_KEY}`,
+      {
+        method: "POST",
+        body: JSON.stringify({
+          idToken: authContext.token,
+          password: enteredNewPassword,
+          returnSecureToken: false,
+        }),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    ).then((data) => console.log(data));
+  };
   return (
-    <form className={classes.form}>
+    <form className={classes.form} onSubmit={submitHandler}>
       <div className={classes.control}>
-        <label htmlFor='new-password'>New Password</label>
-        <input type='password' id='new-password' />
+        <label htmlFor="new-password">New Password</label>
+        <input
+          minLength="7"
+          ref={newPassRef}
+          type="password"
+          id="new-password"
+        />
       </div>
       <div className={classes.action}>
         <button>Change Password</button>
       </div>
     </form>
   );
-}
+};
 
 export default ProfileForm;
