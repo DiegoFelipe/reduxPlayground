d44c4bd294cf41e573b18b31aebd5dfd93bb615f
Diego Felipe <diegofelipe.14@gmail.com>
2022-08-04 10:29:47 -0400

    Testing user interactions

--- testing/src/components/Greeting.js
+++ testing/src/components/Greeting.js
@@ -1,10 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Greeting = () => {
+  const [changeText, setChangeText] = useState(false);
+
+  const upperCaseHandler = (event) => {
+    event.preventDefault();
+    setChangeText(() => !changeText);
+  };
   return (
     <div>
       <h2>Hello World</h2>
-      <p>It's good to see you!</p>
+      {!changeText && <p>It's good to see you!</p>}
+      {changeText && <p>Another text</p>}
+      <button onClick={upperCaseHandler}>Change text</button>
     </div>
   );
 };
--- testing/src/components/Greeting.test.js
+++ testing/src/components/Greeting.test.js
@@ -1,7 +1,24 @@
 import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 import Greeting from "./Greeting";
 
-test("Checks if we have a hello world", () => {
-  render(<Greeting />);
-  expect(screen.getByText(/hello world/i)).toBeInTheDocument();
+describe("Greeting component test", () => {
+  test("Checks if we have a hello world", () => {
+    render(<Greeting />);
+    expect(screen.getByText(/hello world/i)).toBeInTheDocument();
+  });
+  test("Check if the text is changing when clicking", () => {
+    render(<Greeting />);
+    expect(screen.getByText(/It's good to see you!/i)).toBeInTheDocument();
+    userEvent.click(screen.getByRole("button"));
+    expect(screen.getByText(/Another text/i)).toBeInTheDocument();
+  });
+  test('Checking if text "It is good to see you" is hidden when clicking', () => {
+    render(<Greeting />);
+    expect(screen.getByText(/It's good to see you!/i)).toBeInTheDocument();
+    userEvent.click(screen.getByRole("button"));
+    expect(
+      screen.queryByText(/It's good to see you!/i)
+    ).not.toBeInTheDocument();
+  });
 });
